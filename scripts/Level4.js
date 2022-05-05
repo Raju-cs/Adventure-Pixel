82765265ed0de425341cc813f2ab456d1d436b3c
Md.Moinul hasan Raju <84829433+Raju-cs@users.noreply.github.com>
2022-05-05 21:34:47 +0600

    update level4

--- scripts/Level4.js
+++ scripts/Level4.js
@@ -66,25 +66,23 @@ export default class Level_4 extends Phaser.Scene{
           this.isGameStart = true;
           this.hadleCollision();
           this.creatRockHead_one();
+          this.sawAnims2(510,95,1500,800);
+          this.sawAnims2(135,306,1500,250);
+          this.createAnimSpikedBall2(0,7,17,27,37,47,67,190,345);
+          
     }
    
     hadleCollision(){
-     
-        this.physics.add.collider(this.player,  this.map.getGroundLayer());
-        
-        
-      }
-
-  
-      creatRockHead_one(){
+     this.physics.add.collider(this.player,  this.map.getGroundLayer());
+    }
+     creatRockHead_one(){
         let rockHead = this.add.sprite(600,296,"rockHead");
-        
         this.physics.world.enable(rockHead);
             rockHead.anims.play("blink", true);
             this.tweens.add({
               targets: rockHead,
               x: 340,
-              duration: 3000,
+              duration: 2000,
               ease: "power1",
               yoyo: true,
               repeat: -1,
@@ -114,6 +112,64 @@ export default class Level_4 extends Phaser.Scene{
            
           });
       }
+      sawAnims2(sawWidth, sawHeight,duration,X){
+        this.anims.create({
+            key: "sawOn_play",
+            frames: this.anims.generateFrameNumbers('sawOn', { frames: [0, 1, 2, 3, 4, 5, 6, 7] }),
+            frameRate: 8,
+            repeat: -1,
+          });
+    
+       this.sawOn = this.add.sprite(sawWidth,sawHeight,"sawOff");
+       this.sawOn.anims.play("sawOn_play", true);
+       this.physics.world.enable(this.sawOn);
+        this.tweens.add({
+            targets: this.sawOn,
+            x: X,
+            duration: duration,
+            ease: "Linear",
+            yoyo: true,
+            repeat: -1,
+          });
+          this.physics.add.overlap(this.player,   this.sawOn ,()=>{
+            // this.gameOver();
+            console.log("dead");
+          });
+      }
+
+      createAnimSpikedBall2(chain_height1,chain_height2,chain_height3,chain_height4,chain_height5,chain_height6,spikeBall_width,container_width,container_height){
+             
+        let chain_one =  this.add.image(0,chain_height1,'chain').setScale(1.3);  
+        let chain_two =  this.add.image(0,chain_height2,'chain').setScale(1.3);
+        let chain_three =  this.add.image(0,chain_height3,'chain').setScale(1.3);
+        let chain_four =  this.add.image(0,chain_height4,'chain').setScale(1.3);
+        let chain_five =  this.add.image(0,chain_height5,'chain').setScale(1.3);
+        let chain_six =  this.add.image(0,chain_height6,'chain').setScale(1.3);
+        let spikeBall = this.add.image(0,spikeBall_width,'Spikeball');
+        let container = this.add.container(container_width, container_height);
+         container.add([spikeBall,chain_one,chain_two,chain_three,chain_four,chain_five,chain_six]);
+      
+         var tween = this.tweens.addCounter({
+           from: -60,
+           to: 60,
+           duration: 2000,
+           repeat: -1,
+           ease: "Linear",
+           yoyo:true,
+           
+           onUpdate: function (tween)
+           {
+               //  tween.getValue = range between 0 and 360
+   
+               container.setAngle(tween.getValue());
+           }
+       });
+       this.physics.world.enable(spikeBall);
+       this.physics.add.overlap(this.player,   spikeBall ,()=>{
+         this.gameOver();
+       });
+   }
+  
 
     update(){
         if(this.isGameStart){
